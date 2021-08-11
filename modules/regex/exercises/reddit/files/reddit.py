0fb50e9ca25c076618ad2f01aa4f62cdf4f7f356
Ed Summers <ehs@pobox.com>
2021-08-11 08:13:10 -0400

    speed up build by having it track mtimes

--- modules/regex/exercises/reddit/files/reddit.py
+++ modules/regex/exercises/reddit/files/reddit.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Collect the last year of UMD Reddit data from PushShift API.
+"""
+
 import csv
 import time
 import requests
--- scripts/build.py
+++ scripts/build.py
@@ -1,31 +1,81 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 import shutil
+import subprocess
 
 from re import sub
 from os import walk
-from os.path import dirname, abspath, join
-from subprocess import run
+from os.path import dirname, abspath, join, getmtime, isfile
 
+# base directory for the git repository clone
 repo_dir = dirname(dirname(abspath(__file__)))
 
+# test to see if asciidoctor is available
 if not shutil.which('asciidoctor'):
     sys.exit('ERROR: asciidoctor is not installed or is not in your PATH')
 
-def asciidoc(adoc_file):
-    html_file = sub(r'\.adoc$', '.html', adoc_file)
-    cmd = ['asciidoctor', adoc_file, '-o', html_file]
-    result = run(cmd, capture_output=True)
-    if result.stderr:
-        print(f"{adoc_file} - {result.stderr.decode('utf8')}") 
-
 def main():
+    """
+    Walk through the repository directory looking for asciidoc files
+    to convert to HTML. Only files with the .adoc file extension that 
+    have been modified since the last run will be converted.
+    """
+    history = History()
     for dir_name, dirs, files in walk(repo_dir):
         for filename in files:
             if filename.endswith('.adoc'):
-                asciidoc(join(dir_name, filename))
+                path = join(dir_name, filename)
+                if history.is_modified(path):
+                    asciidoc(path)
+
+def asciidoc(adoc_file):
+    """
+    Convert an asciidoc file to HTML using asciidoctor or asciidoctor-revealjs.
+    Returns True if no errors or warnings were generated and False if they 
+    were.
+    """
+
+    html_file = sub(r'\.adoc$', '.html', adoc_file)
+    if adoc_file == "slides.adoc":
+        cmd = ['asciidoctor-revealjs', adoc_file, '-o', html_file]
+    else:
+        cmd = ['asciidoctor', adoc_file, '-o', html_file]
+    result = subprocess.run(cmd, capture_output=True)
+    if result.stderr:
+        print(f"{adoc_file} - {result.stderr.decode('utf8')}") 
+        return False
+    else:
+        return True
+
+class History:
+
+    def __init__(self):
+        self.file = join(repo_dir, '.history')
+        if isfile(self.file):
+            self.mtimes = json.load(open(self.file))
+        else:
+            self.mtimes = {}
+
+    def is_modified(self, path):
+        mtime = getmtime(path)
+        if path in self.mtimes and mtime > self.mtimes[path]:
+            result = True
+        elif path not in self.mtimes:
+            result = True
+        else:
+            result = False
+
+        if result:
+            self.mtimes[path] = mtime
+            self.write()
+        
+        return result
+
+    def write(self):
+        with open(self.file, 'w') as fh:
+            json.dump(self.mtimes, fh, indent=2)
 
 if __name__ == "__main__":
     main()
-                
